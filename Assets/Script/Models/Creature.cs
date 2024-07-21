aaea1f3e64d895adf34cdc8e9d96a44b7ecb135e
Honti Szabolcs <edmond6656@gmail.com>
2024-07-21 17:57:16 +0200

    #2 Creature model létrehozása

---
+++ Assets/Script/Models/Creature.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+using Script.Models.Interfaces;
+
+namespace Script.Models
+{
+    public class Creature : Monster
+    {
+        public Vector2 Position { get; set; }
+
+        public Creature(string uuid, Vector2 position) {
+            Uuid = uuid;
+            GetDataByUuid();
+
+            Position = position;
+        }
+
+        public void Serialize() {
+            throw new NotImplementedException("Script.Models.Creature Serialize is not implemented yet");
+        }
+    }
+}
