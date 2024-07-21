f2f56bc2e2fde376978e432927e1e2918e486af4
Honti Szabolcs <edmond6656@gmail.com>
2024-07-21 18:48:30 +0200

    #2 Player model létrehozása

---
+++ Assets/Script/Models/Player.cs
@@ -0,0 +1,31 @@
+﻿using Script.Models.Interfaces;
+
+namespace Script.Models
+{
+    public class Player : IEntity
+    {
+        public string Uuid { get; set; }
+        public string Name { get; set; }
+        public int MaxHealth { get; set; }
+        public int CurrentHealth { get; set; }
+        public int InitiativeModifier { get; set; }
+        public int ArmorClass { get; set; }
+        public int DoDamage(int damage) {
+            CurrentHealth -= damage;
+            if (CurrentHealth < 0) {
+                CurrentHealth = 0;
+            }
+
+            return CurrentHealth;
+        }
+
+        public int Heal(int amount) {
+            CurrentHealth += amount;
+            if (CurrentHealth > MaxHealth) {
+                CurrentHealth = MaxHealth;
+            }
+
+            return CurrentHealth;
+        }
+    }
+}
