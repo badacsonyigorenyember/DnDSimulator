84657f5ccec6cfe504ccedd542082350869ac1ad
Honti Szabolcs <edmond6656@gmail.com>
2024-07-22 20:38:08 +0200

    #2 CreatureData struct helyett class

---
+++ Assets/Script/Utils/Data/CreatureData.cs
@@ -0,0 +1,11 @@
+using System.Numerics;
+using Script.Utils.Interfaces;
+
+namespace Script.Utils.Data
+{
+    public class CreatureData : IEntityData
+    {
+        public string uuid;
+        public Vector2 position;
+    }
+}
