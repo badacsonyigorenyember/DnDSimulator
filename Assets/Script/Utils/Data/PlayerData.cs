9f783017fa1b975dda7b7aaa6de17d75711d3f77
Honti Szabolcs <edmond6656@gmail.com>
2024-07-22 22:01:19 +0200

    #2 EntityBehaviour létrehozása

---
+++ Assets/Script/Utils/Data/PlayerData.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+using Script.Utils.Interfaces;
+
+namespace Script.Utils.Data
+{
+    public class PlayerData : IEntityData
+    {
+        public string uuid;
+        public Vector2 position;
+    }
+}
