cc75cda88b1d9e5397f652fab40f5bb8b4e4ca04
Honti Szabolcs <edmond6656@gmail.com>
2024-07-22 22:00:20 +0200

    #2 PlayerBehaviour létrehozása

---
+++ Assets/Script/Utils/PlayerBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Script.Models;
+using Script.Utils.Data;
+using Script.Utils.Interfaces;
+
+namespace Script.Utils
+{
+    [Serializable]
+    public class PlayerBehaviour : EntityBehaviour
+    {
+        public override void Init(IEntityData data) {
+            PlayerData playerData = data as PlayerData;
+            Entity = new Player(playerData, true);
+        }
+    }
+}
