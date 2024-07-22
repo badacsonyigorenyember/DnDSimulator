814b35ec1608ae7ae64f400c25ecab0fc29de4b8
Honti Szabolcs <edmond6656@gmail.com>
2024-07-22 20:29:31 +0200

    #2 IEntity bővítése GetDataByUuid-val

--- Assets/Script/Models/Interfaces/IEntity.cs
+++ Assets/Script/Models/Interfaces/IEntity.cs
@@ -10,8 +10,11 @@ namespace Script.Models.Interfaces
         public int CurrentHealth { get; set; }
         public int InitiativeModifier { get; set; }
         public int ArmorClass { get; set; }
+        public bool Visible { get; set; }
 
         public int DoDamage(int damage);
         public int Heal(int amount);
+
+        public void GetDataByUuid(string uuid);
     }
 }
