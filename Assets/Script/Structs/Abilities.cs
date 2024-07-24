e2aa7f0161a249f32fa4861dc19a71a134ea5900
Honti Szabolcs <edmond6656@gmail.com>
2024-07-24 18:55:47 +0200

    # Abilities struct létrehozása

---
+++ Assets/Script/Structs/Abilities.cs
@@ -0,0 +1,12 @@
+﻿namespace Script.Structs
+{
+    public struct Abilities
+    {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Charisma { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Constitution { get; set; }
+    }
+}
