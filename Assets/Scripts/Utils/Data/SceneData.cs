1cd4a436a66e7fa6a508dc18e6e0753f2d06f9bf
Honti Szabolcs <edmond6656@gmail.com>
2024-07-25 19:38:00 +0200

    #2 SceneData létrehozása

---
+++ Assets/Scripts/Utils/Data/SceneData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils.Data
+{
+    public class SceneData
+    {
+        public string Name { get; set; }
+        public List<string> Creatures { get; set; }
+        public List<string> Players { get; set; }
+        public float ZoomScale { get; set; }
+        public Vector2 CamPosition { get; set; }
+    }
+}
