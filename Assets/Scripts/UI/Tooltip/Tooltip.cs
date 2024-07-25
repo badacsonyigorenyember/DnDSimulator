f7dc0a9bc60403afade0f5d141803169f089c471
Honti Szabolcs <edmond6656@gmail.com>
2024-07-25 18:35:04 +0200

    fix-some-namespace fix Tooltip namespace

--- Assets/Scripts/UI/Tooltip/Tooltip.cs
+++ Assets/Scripts/UI/Tooltip/Tooltip.cs
@@ -3,52 +3,55 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-[ExecuteInEditMode()]
-public class Tooltip : MonoBehaviour
+namespace UI.Tooltip
 {
-    [SerializeField] private TextMeshProUGUI _headerText;
-    [SerializeField] private TextMeshProUGUI _contentText;
+    [ExecuteInEditMode()]
+    public class Tooltip : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _headerText;
+        [SerializeField] private TextMeshProUGUI _contentText;
 
-    [SerializeField] private LayoutElement _layoutElement;
-    [SerializeField] private CanvasGroup gp;
+        [SerializeField] private LayoutElement _layoutElement;
+        [SerializeField] private CanvasGroup gp;
 
-    [SerializeField] private int _characterWrapLimit;
+        [SerializeField] private int _characterWrapLimit;
 
-    [SerializeField] private float _fadeDuration;
+        [SerializeField] private float _fadeDuration;
 
-    public void SetText(string header, string content = "") {
-        _headerText.text = header;
+        public void SetText(string header, string content = "") {
+            _headerText.text = header;
 
-        if (!string.IsNullOrEmpty(content)) {
-            _contentText.gameObject.SetActive(true);
-            _contentText.text = content;
-        }
-        else {
-            _contentText.gameObject.SetActive(false);
-        }
+            if (!string.IsNullOrEmpty(content)) {
+                _contentText.gameObject.SetActive(true);
+                _contentText.text = content;
+            }
+            else {
+                _contentText.gameObject.SetActive(false);
+            }
 
-        int headerLength = _headerText.text.Length;
-        int contentLength = _contentText.text.Length;
+            int headerLength = _headerText.text.Length;
+            int contentLength = _contentText.text.Length;
 
-        _layoutElement.enabled = headerLength > _characterWrapLimit || contentLength > _characterWrapLimit;
-    }
-
-    public void StartFade(bool value) {
-        StartCoroutine(Fade(value));
-    }
-
-    public IEnumerator Fade(bool value) {
-        float elapsedTime = 0f;
-
-        float start = gp.alpha;
-        float end = value ? 1 : 0;
-
-        while (elapsedTime < _fadeDuration) {
-            elapsedTime += Time.deltaTime;
-            gp.alpha = Mathf.Lerp(start, end, elapsedTime / _fadeDuration);
-            yield return null;
+            _layoutElement.enabled = headerLength > _characterWrapLimit || contentLength > _characterWrapLimit;
         }
 
-        gp.alpha = end;
+        public void StartFade(bool value) {
+            StartCoroutine(Fade(value));
+        }
+
+        public IEnumerator Fade(bool value) {
+            float elapsedTime = 0f;
+
+            float start = gp.alpha;
+            float end = value ? 1 : 0;
+
+            while (elapsedTime < _fadeDuration) {
+                elapsedTime += Time.deltaTime;
+                gp.alpha = Mathf.Lerp(start, end, elapsedTime / _fadeDuration);
+                yield return null;
+            }
+
+            gp.alpha = end;
+        }
     }
 }
