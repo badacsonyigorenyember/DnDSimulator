5facc27d050603f3d4beac1d12482ac49af79451
Honti Szabolcs <edmond6656@gmail.com>
2024-07-25 18:58:35 +0200

    #2 SceneData fix

--- Assets/Scripts/Utils/SceneData.cs
+++ Assets/Scripts/Utils/SceneData.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Models;
 using UnityEngine;
 
 namespace Utils
@@ -6,13 +7,15 @@ namespace Utils
     public class SceneData
     {
         public string name;
-        public List<string> creatures;
+        public List<Creature> creatures;
+        public List<Player> players;
         public float zoomScale;
         public Vector2 camPosition;
 
         public SceneData(string name) {
             this.name = name;
-            creatures = new List<string>();
+            creatures = new List<Creature>();
+            players = new List<Player>();
         }
     }
 }
