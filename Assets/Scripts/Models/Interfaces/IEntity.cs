f58e3645818ff3793b01d82291a8c0900ce9499f
Honti Szabolcs <edmond6656@gmail.com>
2024-07-26 21:31:23 +0200

    #2 IEntity change

--- Assets/Scripts/Models/Interfaces/IEntity.cs
+++ Assets/Scripts/Models/Interfaces/IEntity.cs
@@ -1,17 +1,19 @@
-﻿namespace Models.Interfaces
+﻿using UnityEngine;
+
+namespace Models.Interfaces
 {
     public interface IEntity
     {
+        public string Uuid { get; set; }
         public string Name { get; set; }
         public int MaxHealth { get; set; }
         public int CurrentHealth { get; set; }
         public int InitiativeModifier { get; set; }
         public int ArmorClass { get; set; }
         public bool Visible { get; set; }
+        public Vector2 Position { get; set; }
 
         public int DoDamage(int damage);
         public int Heal(int amount);
-
-        public void GetDataByUuid(string uuid);
     }
 }
